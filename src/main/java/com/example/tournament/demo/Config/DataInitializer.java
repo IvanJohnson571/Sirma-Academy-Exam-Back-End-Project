0cd7ed84b5a628d3e88322c19b5e1c6307d9177f
Ivan <78429506+ivanjohnson571@users.noreply.github.com>
2024-09-09 15:12:43 +0300

    New initial commit. Started on new branch and project due to some unresolved bug in the previous project.

---
+++ src/main/java/com/example/tournament/demo/Config/DataInitializer.java
@@ -0,0 +1,35 @@
+package com.example.tournament.demo.Config;
+
+import com.example.tournament.demo.Model.Team.Team;
+import com.example.tournament.demo.Repository.TeamRepository.TeamRepository;
+import com.example.tournament.demo.Service.CsvReaderService.CsvReaderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Autowired
+    private CsvReaderService csvReaderService;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Четене на данни от CSV файла
+        List<Team> teams = csvReaderService.readTeamsCsv("src/main/resources/csv_files/teams.csv");
+
+        // Записване на отборите в H2 базата данни
+        for (Team team : teams) {
+            teamRepository.save(team);
+        }
+
+        // Потвърждение в конзолата
+        System.out.println("Teams have been initialized in the database!");
+    }
+
+}
---
+++ src/main/java/com/example/tournament/demo/TournamentApplication.java
@@ -0,0 +1,13 @@
+package com.example.tournament.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentApplication.class, args);
+	}
+
+}
